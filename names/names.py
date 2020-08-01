e011a9f10d077a966d51192d44847f3caeedb3bf
Jordan Devaney <jordandevaney@Jordans-MacBook-Pro.local>
2020-08-01 15:18:51 -0400

    I AM A GIANT STUPID FRAUD

--- names/names.py
+++ names/names.py
@@ -26,19 +26,27 @@ duplicates = []
     #     if name_1 == name_2:
     #         stack.push(name_1)
 
+# HOW DO I INSERT STRINGS INTO A BSTNode
+
+
 # send list number 1 and start out at index 0
-first_name_list = BSTNode(names_1[0])
-print(first_name_list.value)
-for i in range(1, len(names_1)):
-    # take each name from this list and insert into the tree
-    print(i)
-    # take result of this and compare it
-    
+first_name_list = BSTNode(names_1)
+
 # for list 2 compare
 # compare list 1 with list 2 with the contains method on the BSTNode Class
+second_name_list = BSTNode(names_2)
 
-second_name_list = BSTNode(names_2[0])
 
+for i in range(1, len(names_1)):
+    # take each name from this list and insert into the tree
+    first_name_list.insert((names_1[i]).value)
+    # take result of this and compare it
+for i in range(1, len(names_2)):
+    second_name_list.insert((names_2[i]).value)
+    
+print(first_name_list)
+print(second_name_list)
+    
 end_time = time.time()
 # print (f"{len(stack)} duplicates:\n\n{', '.join(stack)}\n\n")
 # print (f"runtime: {end_time - start_time} seconds")
