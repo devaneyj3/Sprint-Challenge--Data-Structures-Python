4291f51ff1889d1c20717a7f5331952064c76579
Jordan Devaney <jordandevaney@Jordans-MacBook-Pro.local>
2020-07-31 22:10:37 -0400

    passed some test but 2 on the ring-buffer test

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,25 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.data = []
 
     def append(self, item):
-        pass
+        # length can't be more than 5
+        if len(self.data) == self.capacity:
+            # insert the new elment at position 0
+            self.data.insert(5, item)
+            # get rid of last elment
+            self.data.pop(0)
+        else: 
+            self.data.append(item)
+        return self.data
 
     def get(self):
-        pass
+        return self.data
+    
+
+buffer = RingBuffer(5)
+for i in range(50):
+    buffer.append(i)
+
+print(buffer.get()) 
